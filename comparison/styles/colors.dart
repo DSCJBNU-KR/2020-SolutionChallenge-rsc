4435b2c513cad4b32de6520b34cf74e44eabc09e
JeongyoonLee <43804152+jeongyoonlee2015@users.noreply.github.com>
2020-04-06 11:04:09 +0900

    Create colors.dart

---
+++ comparison/styles/colors.dart
@@ -0,0 +1,22 @@
+/*
+*  colors.dart
+*  JBNU 1st
+*
+*  Created by .
+*  Copyright © 2018 . All rights reserved.
+    */
+
+import 'dart:ui';
+
+
+class AppColors {
+  static const Color primaryBackground = Color.fromARGB(255, 255, 255, 255);
+  static const Color secondaryBackground = Color.fromARGB(255, 242, 242, 243);
+  static const Color ternaryBackground = Color.fromARGB(255, 32, 217, 148);
+  static const Color primaryElement = Color.fromARGB(255, 216, 216, 216);
+  static const Color secondaryElement = Color.fromARGB(255, 32, 217, 148);
+  static const Color accentElement = Color.fromARGB(255, 157, 157, 157);
+  static const Color primaryText = Color.fromARGB(255, 157, 157, 157);
+  static const Color secondaryText = Color.fromARGB(255, 23, 49, 67);
+  static const Color accentText = Color.fromARGB(255, 255, 255, 255);
+}
