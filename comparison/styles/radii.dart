3813d53400259ecd23cbdb0f9ddbb7cf3047d58c
JeongyoonLee <43804152+jeongyoonlee2015@users.noreply.github.com>
2020-04-06 11:04:25 +0900

    Create radii.dart

---
+++ comparison/styles/radii.dart
@@ -0,0 +1,14 @@
+/*
+*  radii.dart
+*  JBNU 1st
+*
+*  Created by .
+*  Copyright © 2018 . All rights reserved.
+    */
+
+import 'package:flutter/rendering.dart';
+
+
+class Radii {
+  static const BorderRadiusGeometry k4pxRadius = BorderRadius.all(Radius.circular(4));
+}
